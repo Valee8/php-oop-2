3a7c83fc745784109dedc8ce548be4115967f267
Vale <oirelav95@gmail.com>
2023-01-25 23:44:21 +0100

    aggiungo metodi

--- index.php
+++ index.php
@@ -6,5 +6,17 @@
         public $categoria;
         public $prezzo;
 
+        public function __construct($nome, $categoria, $prezzo) {
+            $this -> nome = $nome;
+            $this -> categoria = $categoria;
+            $this -> prezzo = $prezzo;
+        }
+
+        public function getProduct() {
+            return "<div class='card'>Nome prodotto: " . $this -> nome . 
+            "</div>" . "<div class='card'>Categoria: " . $this -> categoria .
+            "</div>" . "<div class='card'>Prezzo: " . $this -> prezzo . "</div>";
+        }
+
     }
 ?>
