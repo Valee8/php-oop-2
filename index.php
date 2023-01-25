72371382dc313d9eebededa355c618296176aa6b
Vale <oirelav95@gmail.com>
2023-01-25 23:45:33 +0100

    aggiungo html

--- index.php
+++ index.php
@@ -1,36 +1,49 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - OOP2</title>
+</head>
+<body>
 
-    class Prodotto {
+    <?php
 
-        public $nome;
-        public $categoria;
-        public $prezzo;
+        class Prodotto {
 
-        public function __construct($nome, $categoria, $prezzo) {
-            $this -> nome = $nome;
-            $this -> categoria = $categoria;
-            $this -> prezzo = $prezzo;
+            public $nome;
+            public $categoria;
+            public $prezzo;
+
+            public function __construct($nome, $categoria, $prezzo) {
+                $this -> nome = $nome;
+                $this -> categoria = $categoria;
+                $this -> prezzo = $prezzo;
+            }
+
+            public function getProduct() {
+                return "<div class='card'>Nome prodotto: " . $this -> nome . 
+                "</div>" . "<div class='card'>Categoria: " . $this -> categoria .
+                "</div>" . "<div class='card'>Prezzo: " . $this -> prezzo . "</div>";
+            }
+
         }
 
-        public function getProduct() {
-            return "<div class='card'>Nome prodotto: " . $this -> nome . 
-            "</div>" . "<div class='card'>Categoria: " . $this -> categoria .
-            "</div>" . "<div class='card'>Prezzo: " . $this -> prezzo . "</div>";
+        $prodotti = [
+            new Prodotto("Cibo", "Cane", 10.50),
+            new Prodotto("Lettiera", "Gatto", 8.90),
+            new Prodotto("Cuccia", "Cane", 20)
+        ];
+
+        echo "<div class='cards'>";
+
+        foreach($prodotti as $prodotto) {
+            echo $prodotto -> getProduct();
         }
 
-    }
-
-    $prodotti = [
-        new Prodotto("Cibo", "Cane", 10.50),
-        new Prodotto("Lettiera", "Gatto", 8.90),
-        new Prodotto("Cuccia", "Cane", 20)
-    ];
-
-    echo "<div class='cards'>";
-
-    foreach($prodotti as $prodotto) {
-        echo $prodotto -> getProduct();
-    }
-
-    echo "</div>";
-?>
+        echo "</div>";
+    ?>
+    
+</body>
+</html>
