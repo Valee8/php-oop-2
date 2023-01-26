004829b0fb024774750b3f6ff953d30f14198cbf
Vale <oirelav95@gmail.com>
2023-01-26 19:48:10 +0100

    modifico aggiungendo altre classi

--- index.php
+++ index.php
@@ -11,44 +11,115 @@
 
     <?php
 
-        class Prodotto {
+        class Product {
 
-            public $nome;
-            public $categoria;
-            public $prezzo;
+            private $name;
+            private $category;
+            private $price;
 
-            public function __construct($nome, $categoria, $prezzo) {
-                $this -> nome = $nome;
-                $this -> categoria = $categoria;
-                $this -> prezzo = $prezzo;
+            public function __construct($name, $category, $price) {
+                $this -> setName($name);
+                $this -> setCategory($category);
+                $this -> setPrice($price);
             }
 
             public function getProduct() {
-                return "<div class='card'>" .
-                    "<div class='card-info'>Nome prodotto: " . $this -> nome . "</div>" . 
-                    "<div class='card-info'>Categoria: " . $this -> categoria . "</div>" . 
-                    "<div class='card-info'>Prezzo: " . $this -> prezzo . "&euro;</div>" . 
-                    "</div>";
+                return "<div class='card-info'>Nome prodotto: " . $this -> getName() . "</div>" . 
+                    "<div class='card-info'>Categoria: " . $this -> getCategory() . "</div>" . 
+                    "<div class='card-info'>Prezzo: " . $this -> getPrice() . "&euro;</div>";
+            }
+
+            public function setName($name) {
+                $this -> name = $name;
+            }
+
+            public function getName() {
+                return $this -> name;
+            }
+
+            public function setCategory($category) {
+                $this -> category = $category;
+            }
+
+            public function getCategory() {
+                return $this -> category;
+            }
+
+            public function setPrice($price) {
+                $this -> price = $price;
+            }
+
+            public function getPrice() {
+                return $this -> price;
             }
 
         }
 
-        $prodotti = [
-            new Prodotto("Croccantini", "Cane", 10.50),
-            new Prodotto("Lettiera", "Gatto", 8.90),
-            new Prodotto("Cuccia", "Cane", 20),
-            new Prodotto("Cuscino", "Gatto", 10),
-            new Prodotto("Gomitolo", "Gatto", 15),
-            new Prodotto("Guinzaglio", "Cane", 8.20)
+        class Food extends Product {
+
+            private $expirationDate;
+
+            public function __construct($name, $category, $price, $expirationDate) {
+                parent :: __construct($name, $category, $price);
+
+                $this -> setExpiration($expirationDate);
+            }
+
+            public function setExpiration($expirationDate) {
+                $this -> expirationDate = $expirationDate;
+            }
+
+            public function getExpiration() {
+                return "<div class='card-info'>Data scadenza: " . $this -> expirationDate . "</div>";
+            }
+
+            public function getProduct() {
+                return parent :: getProduct() . $this -> getExpiration();
+            }
+            
+        }
+
+        class Toy extends Product {
+
+            public function __construct($name, $category, $price) {
+                parent :: __construct($name, $category, $price);
+            }
+
+            public function getProduct() {
+                return parent :: getProduct();
+            }
+            
+        }
+
+        class Bed extends Product {
+
+            public function __construct($name, $category, $price) {
+                parent :: __construct($name, $category, $price);
+            }
+
+            public function getProduct() {
+                return parent :: getProduct();
+            }
+            
+        }
+
+        $toy = new Toy("Gomitolo", "Gatto", 8.90);
+        $food = new Food("Croccantini", "Cane", 10.50, "10-02-2023");
+        $bed = new Bed("Cuccia", "Cane", 10.50, 20);
+
+        $products = [
+            $toy, $food, $bed
         ];
 
+    
         echo "<div class='cards'>";
 
-        foreach($prodotti as $prodotto) {
-            echo $prodotto -> getProduct();
+        foreach($products as $product) {
+            echo "<div class='card'>" . $product -> getProduct() . "</div>";
         }
 
         echo "</div>";
+
     ?>
     
 </body>
